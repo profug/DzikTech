97d5055d098206cf7943dc1f643bd7bf5b6ec95f
profug <39553508+profug@users.noreply.github.com>
2021-03-20 07:41:27 -0700

    Add files via upload

---
+++ NumberOneClass.java
@@ -0,0 +1,13 @@
+package NumberOne;
+
+import java.awt.Graphics;
+
+import javax.swing.JApplet;
+
+public class NumberOneClass extends JApplet {
+	
+		public void paint(Graphics g) {
+			super.paint(g);
+			g.drawString("Wow, this is a perfect one", 25, 25);
+		}
+}
